f016b86601b4edcc42685edbcfd8b00ba95d2cfb
Matei Alexandru Onea <matei.onea@gmail.com>
2021-10-23 17:57:01 +0300

    Fashion_blog_LP
    Rotating nav

---
+++ 3rd project/blogScript.js
@@ -0,0 +1,9 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+const lists = document.querySelectorAll('.list')
+
+
+open.addEventListener('click', () => container.classList.add('show-nav'))
+close.addEventListener('click', () => container.classList.remove('show-nav'))
+
