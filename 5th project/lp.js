1d4d1b5e19c33101d54f980e4a7fc36d118df2ae
Matei Alexandru Onea <matei.onea@gmail.com>
2021-10-28 20:19:48 +0300

    Icons added
    LinkedIn + Facebook + Github added

--- 5th project/lp.js
+++ 5th project/lp.js
@@ -1,5 +1,7 @@
 const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
+const icons = document.querySelectorAll('.elements')
+
 
 let load = 0
 
@@ -11,12 +13,31 @@ function blurring() {
   if (load == 100) {
     clearInterval(interval)
   }
-
   loadText.innerText = `${load}%`
   loadText.style.opacity = scale(load, 0, 100, 1, 0)
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+  icons.forEach(icon => {
+    icon.style.opacity = scale(load, 0, 100, 0, 1)
+  });
+  
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
